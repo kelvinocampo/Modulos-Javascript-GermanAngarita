a8a3cef324e165ef4a1a69e236f97a0f6505ed0f
davidar27 <jhonatanarcos10@gmail.com>
2024-04-29 14:48:56 -0500

    dd

--- Modulos/Services/tiempo/minhor.js
+++ Modulos/Services/tiempo/minhor.js
@@ -3,7 +3,7 @@ import { minhorFinal } from './minhorFinal.js'
 
 export function minhor() {
     let menu = `
-    Digite la cantidad de decimetros a convertir`
+    Digite la cantidad de minutos a convertir`
     let cantidad = parseInt(prompt(menu))
     if (numeroValido(cantidad)) {
         let resultado = cantidad / 10
