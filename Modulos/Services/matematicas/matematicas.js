3acd0263847a221236f44ab006fc1f47d83c9433
kelvinocampo <kevinocampooso@gmail.com>
2024-04-29 14:55:47 -0500

    a

--- Modulos/Services/matematicas/matematicas.js
+++ Modulos/Services/matematicas/matematicas.js
@@ -1,20 +1,28 @@
-import { mainMenu } from './../../main'
+import { mainMenu } from "./../../main.js";
+import { mostrar } from './../mostrar.js'
 
 export function matematicas() {
     let menu = `
     Digite,
-    1 Para convertir PEPEP nuevamente
-    m a km
-    2 Para volver al menu principal
+    1 Para multiplicar
+    2 Para dividir
+    3 Para restar
+    4 Para sumar
     ...
-    0 Para salir`
+    0 Para devolverse`
     let opcion = parseInt(prompt(menu))
     switch (opcion) {
         case 1:
-            mkm()
+            multiplicar()
             break;
         case 2:
-            dmm()
+            dividir()
+            break;
+        case 3:
+            restar()
+            break;
+        case 4:
+            sumar()
             break;
         case 0:
             mainMenu()
