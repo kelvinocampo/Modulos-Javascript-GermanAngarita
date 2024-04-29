81821b4ccab842fa73e6cf0384cceef2adc55a3a
davidar27 <jhonatanarcos10@gmail.com>
2024-04-29 14:13:52 -0500

    d

--- Modulos/Services/longitud/dmm.js
+++ Modulos/Services/longitud/dmm.js
@@ -3,7 +3,7 @@ import { dmmFinal } from './dmmFinal.js'
 
 export function dmm() {
     let menu = `
-    Digite la cantidad de metros a convertir`
+    Digite la cantidad de decimetros a convertir`
     let cantidad = parseInt(prompt(menu))
     if (numeroValido(cantidad)) {
         let resultado = cantidad / 10
